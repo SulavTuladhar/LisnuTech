88a5bc6e212622d98eba7a9dc602a517f2cf24a9
Sulav Tuladhar <sulavtuladhar@gmail.com>
2021-12-20 18:18:01 +0545

    all

---
+++ Backend/modules/product/product.route.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const productController = require('./product.controller');
+
+// middlewares
+const uploader = require('./../../middlewares/uploader') // Fileupload
+const authenticate = require('../../middlewares/authenticate'); // authenticate
+
+router.route('/')
+    .get(productController.find)
+    .post(authenticate, uploader.single('image'), productController.insert);
+
+router.route('/:id')
+    .get(productController.findById)
+    .put(authenticate, uploader.single('image'), productController.update)
+    .delete(authenticate, productController.remove);
+
+module.exports = router;
